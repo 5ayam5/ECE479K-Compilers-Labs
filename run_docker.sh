600ae1fddfd286a4b7d5dd638af41c8246af8c33
Sayam Sethi <sayam.sethi2@gmail.com>
2024-01-26 13:38:30 -0600

    Lab 1 added

---
+++ run_docker.sh
@@ -0,0 +1,13 @@
+function ece479k_docker() {
+    lab=$1
+    image=$2
+    if [ -z "$lab" ]; then
+        echo "Usage: ece479k_docker <lab> <image>"
+        return
+    fi
+    if [ -z "$image" ]; then
+        echo "Usage: ece479k_docker <lab> <image>"
+        return
+    fi
+    docker run --platform linux/amd64 -it -v "$PWD/$lab":/code $image
+}
