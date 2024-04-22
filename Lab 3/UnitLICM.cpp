15c236f9433bbc0fec9eb27ea5fb24fae516a8b9
jaeyoung-p <jaeyoung@utexas.edu>
2024-04-22 14:07:06 -0500

    UnitLICM added starting code

--- Lab 3/UnitLICM.cpp
+++ Lab 3/UnitLICM.cpp
@@ -11,13 +11,45 @@ using namespace llvm;
 using namespace ece479k;
 
 /// Main function for running the LICM optimization
-PreservedAnalyses UnitLICM::run(Function& F, FunctionAnalysisManager& FAM) {
+PreservedAnalyses UnitLICM::run(Function &F, FunctionAnalysisManager &FAM)
+{
   dbgs() << "UnitLICM running on " << F.getName() << "\n";
   // Acquires the UnitLoopInfo object constructed by your Loop Identification
   // (LoopAnalysis) pass
   UnitLoopInfo &Loops = FAM.getResult<UnitLoopAnalysis>(F);
 
   // Perform the optimization
+  // For each loop in the function
+  for (auto &L : Loops.getLoops())
+  {
+    // For each block in the loop
+    for (auto &BB : L->getBlocks())
+    {
+      // For each instruction in the block
+      for (auto &I : *BB)
+      {
+        // // Check if each operand is loop invariant
+        // bool allLoopInvariant = true;
+        // for (auto &Op : I.operands())
+        // {
+        //   if (Instruction *OpI = dyn_cast<Instruction>(Op))
+        //   {
+        //     if (L->getBlocks().count(OpI->getParent()))
+        //     {
+        //       allLoopInvariant = false;
+        //       break;
+        //     }
+        //   }
+        // }
+        // // If all operands are loop invariant, move the instruction to the loop
+        // // header
+        // if (allLoopInvariant)
+        // {
+        //   I.moveBefore(L->getHeader()->getFirstNonPHI());
+        // }
+      }
+    }
+  }
 
   // Set proper preserved analyses
   return PreservedAnalyses::all();
